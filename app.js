e6b44dcb660c2bbd445abb4e32748f579f3de6e3
winifredy111 <144274684+winifredy111@users.noreply.github.com>
2023-09-06 19:53:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_h9al3dg", "template_rcossoi", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
